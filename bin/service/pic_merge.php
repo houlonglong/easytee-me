3e5485f0b5b6817831a6d85dfe2c00fe7619ee9e
joseph <joseph@qq.com>
2015-08-14 15:36:51 +0800

    i

--- bin/service/pic_merge.php
+++ bin/service/pic_merge.php
@@ -2,7 +2,7 @@
 while(1){
     //执行频率 单位:秒
     $sleep = "2";
-    $cmd = "/usr/bin/php /data/projects/easytee/easytee_v2/bin/cli.php service/pic run";
+    $cmd = "php /data/projects/easytee/easytee_v2/bin/cli.php service/pic run ptenv=product";
     system($cmd,$status);
     echo $cmd.PHP_EOL;
     sleep($sleep);
--- bin/service/pic_merge_test.php
+++ bin/service/pic_merge_test.php
@@ -2,7 +2,7 @@
 while(1){
     //执行频率 单位:秒
     $sleep = "2";
-    $cmd = "php /data/projects/easytee/easytee_v2/bin/cli.php service/pic run test";
+    $cmd = "php /data/projects/easytee/easytee_v2/bin/cli.php service/pic run ptenv=test";
     system($cmd);
     echo $cmd.PHP_EOL;
     sleep($sleep);
