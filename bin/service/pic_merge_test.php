586a86484f95ce65f484b04c870ce1a92686d65d
joseph <joseph@qq.com>
2015-08-14 15:00:09 +0800

    i

--- bin/service/pic_merge_test.php
+++ bin/service/pic_merge_test.php
@@ -2,6 +2,8 @@
 while(1){
     //执行频率 单位:秒
     $sleep = "2";
-    system("php /data/projects/easytee/easytee_v2/bin/service/cli.php service/pic run test");
+    $cmd = "php /data/projects/easytee/easytee_v2/bin/cli.php service/pic run test";
+    system($cmd);
+    echo $cmd.PHP_EOL;
     sleep($sleep);
 }
