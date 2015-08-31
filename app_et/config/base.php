38adbb14f80cf888564ddd05de07e3b29e85a1a1
joseph <joseph@qq.com>
2015-08-31 09:38:50 +0800

    no msg

--- app_et/config/base.php
+++ app_et/config/base.php
@@ -2,3 +2,5 @@
 define("PRE","<pre />");
 #define("SESSION_HANDLER","redis");
 define('CDN_DOMAIN_OPEN', 'http://cdn.open.easytee.me');
+
+define("NEW_DB","new");
--- app_et/config/base/local.php
+++ app_et/config/base/local.php
@@ -1,4 +1,3 @@
 <?php
 define("PT_DEBUG",true);
 define("FRONTEND_DOMAIN",'11.dev.jzw.com');
-define("NEW_DB","new");
