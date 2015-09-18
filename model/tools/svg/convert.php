dc1abb620dc19c0a559a3c105adae650d37b3060
joseph <joseph@qq.com>
2015-09-18 12:06:40 +0800

    deploy

--- model/tools/svg/convert.php
+++ model/tools/svg/convert.php
@@ -48,12 +48,11 @@ class Model_Tools_Svg_Convert extends BaseModel {
             //return $_act_designs;
             foreach($_act_designs as $_act_design){
                 $svg_url = $_act_design['svg_url'];
-                pt_debug($svg_url);
+                //pt_debug($svg_url);
                 $_svg_content = file_get_contents($svg_url);
                 $side = $_act_design['side'];
                 foreach($product_ids as $product_id){
                     $design_info =  $product_designs[$product_id][$side];
-
                     //return $design_info;
                     $x =$design_info['x'];
                     $y =$design_info['y'];
@@ -62,21 +61,23 @@ class Model_Tools_Svg_Convert extends BaseModel {
                     //echo $img_content;
 
                     $img_content = "data:image/png;base64,".base64_encode($img_content);
-
                     $svg_content = "<svg x='".($x/2)."' y='".($y/2)."'".substr($_svg_content,4);
                     $tpl_content = '<svg height="500" width="500" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  style="overflow: hidden; position: relative;" viewBox="0 0 500 500" preserveAspectRatio="xMidYMid meet"><image x="0" y="0" width="500" height="500" preserveAspectRatio="none" xlink:href="' . $img_content . '" transform="matrix(1,0,0,1,0,0)"></image>'.$svg_content.'</svg>';
-                    $local_svg = "/tmp/svg/test_{$product_id}_$side.svg";
-                    $local_png = "/tmp/svg/test_{$product_id}_$side.png";
+                    $name = "{$id}_{$product_id}_{$side}";
+                    $local_svg = "/tmp/svg/test_{$name}.svg";
+                    $local_png = "/tmp/svg/test_{$name}.png";
                     file_put_contents($local_svg,$tpl_content);
 
                     $path_pro = PATH_PRO;
                     $cmd = "python $path_pro/bin/svg/convert.py $local_svg $local_png";
-                    pt_debug($cmd);
+                    //pt_debug($cmd);
                     shell_exec($cmd);
 
                     //continue;
-                    $url = Model_Aliyun_Oss::upload_file($local_png,"test/test/test_{$product_id}_$side.png");
-                    echo $url.PHP_EOL;
+                    $url = Model_Aliyun_Oss::upload_file($local_png,"test/test/$name.png");
+                    @unlink($local_png);
+                    @unlink($local_svg);
+                    //echo $url.PHP_EOL;
                 }
             }
 
