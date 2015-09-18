509ac470d60a7075b391133b89299a5a03fac5e6
joseph <joseph@qq.com>
2015-09-18 11:06:06 +0800

    deploy

--- model/tools/svg/convert.php
+++ model/tools/svg/convert.php
@@ -44,6 +44,7 @@ class Model_Tools_Svg_Convert extends BaseModel {
         $__act_designs = array();
         if(!$_act_product_designs){
             $_act_designs = self::_db()->select_rows("select svg_url,side from design_svg_side where design_id = ?",$act['design_id']);
+            return $_act_designs;
             foreach($_act_designs as $_act_design){
                 $svg_url = $_act_design['svg_url'];
                 $svg_content = file_get_contents($svg_url);
