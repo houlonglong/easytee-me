8d804faed0d3852b0b28b17a83f505458e1a7a9d
joseph <joseph@qq.com>
2015-09-18 11:24:18 +0800

    deploy

--- model/tools/svg/convert.php
+++ model/tools/svg/convert.php
@@ -58,9 +58,11 @@ class Model_Tools_Svg_Convert extends BaseModel {
                     $y =$design_info['y'];
                     $img_url = $design_info['img_url'];
                     $img_content = file_get_contents($img_url);
-                    echo $img_content;
-                    continue;
+                    //echo $img_content;
+
                     $img_content = "data:image/png;base64,".base64_encode($img_content);
+                    echo $img_content.PHP_EOL;
+                    continue;
                     $svg_content = "<svg x='".($x/2)."' y='".($y/2)."'".substr($svg_content,4);
                     $tpl_content = '<svg height="500" width="500" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  style="overflow: hidden; position: relative;" viewBox="0 0 500 500" preserveAspectRatio="xMidYMid meet"><image x="0" y="0" width="500" height="500" preserveAspectRatio="none" xlink:href="' . $img_content . '" transform="matrix(1,0,0,1,0,0)"></image>'.$svg_content.'</svg>';
                     file_put_contents("/tmp/test_$side.svg",$tpl_content);
