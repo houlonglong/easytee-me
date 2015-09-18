f71111b10cee7d9ead42d062ddd2717309c0be23
joseph <joseph@qq.com>
2015-09-18 11:31:27 +0800

    deploy

--- bin/svg/convert.py
+++ bin/svg/convert.py
@@ -1,9 +1,13 @@
+import sys
 import cairo
 import rsvg
+local_svg = sys.argv[1]
+local_png = sys.argv[2]
+
 img = cairo.ImageSurface(cairo.FORMAT_ARGB32, 500,500)
 ctx = cairo.Context(img)
-handle = rsvg.Handle("/tmp/test.svg")
+handle = rsvg.Handle(local_svg)
 # or, for in memory SVG data:
 #handle= rsvg.Handle(None, str(<svg data>))
 handle.render_cairo(ctx)
-img.write_to_png("/tmp/test.png")
+img.write_to_png(local_png)
