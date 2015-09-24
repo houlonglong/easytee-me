b188928183c7585b1d2afd98828ee6336b0bbd16
ihonestlife <haolun0228@qq.com>
2015-09-24 14:48:05 +0800

    静态

--- webroot/js/app/design/main.js
+++ webroot/js/app/design/main.js
@@ -7,68 +7,68 @@ $(function () {
         $('.tab-content').eq(idx).addClass('active');
     });
 
-    $('#upload_location_btn').click(function(){
+    $('#upload_location_btn').click(function () {
         showImageUploadLayer();
     });
 
-    $('#image_store_btn').click(function(){
+    $('#image_store_btn').click(function () {
         showImageStoreLayer();
     });
 
-    $('.design-dropdown-btn').click(function(e){
+    $('.design-dropdown-btn').click(function (e) {
         e.stopPropagation();
         var menu = $(this).siblings('.design-dropdown-menu');
-        $('.design-dropdown-menu').each(function(idx, obj){
-            if(obj != menu[0]){
+        $('.design-dropdown-menu').each(function (idx, obj) {
+            if (obj != menu[0]) {
                 $(obj).hide();
             }
         });
         menu.toggle();
     });
 
-    $('.design-dropdown-menu').click(function(e){
+    $('.design-dropdown-menu').click(function (e) {
         e.stopPropagation();
     });
 
-    $(window).click(function(){
+    $(window).click(function () {
         $('.design-dropdown-menu').hide();
     });
 
-    $('.product-item').click(function(){
+    $('.product-item').click(function () {
         $('.product-item').removeClass('active');
         $(this).addClass('active');
     });
 
-    function showImageLayer(){
+    function showImageLayer() {
         $('.tab-content-image-layout, .tab-content-image-layout-or').show();
         $('.upload-location').hide();
         $('.image-store').hide();
     }
 
-    function showImageUploadLayer(){
+    function showImageUploadLayer() {
         $('.tab-content-image-layout, .tab-content-image-layout-or').hide();
         $('.upload-location').show();
         $('.image-store').hide();
     }
 
-    function showImageStoreLayer(){
+    function showImageStoreLayer() {
         $('.tab-content-image-layout, .tab-content-image-layout-or').hide();
         $('.upload-location').hide();
         $('.image-store').show();
     }
 
-    function setDsHeight(){
+    function setDsHeight() {
         var tools = $('.design-tools');
         var choice = $('.product-choices');
         var design = $('.design-center');
-        if(tools.outerHeight()>choice.outerHeight()){
+        if (tools.outerHeight() > choice.outerHeight()) {
             design.height(tools.outerHeight() + parseInt(tools.css('marginTop')) + 20);
-        }else{
+        } else {
             design.height(choice.outerHeight() + parseInt(choice.css('marginTop')) + 20);
         }
     }
 
-    function initFontFamilies(){
+    function initFontFamilies() {
         var path = '/designer/fonts';
         var fontType = 'popular';
         var fonts = [
@@ -86,23 +86,23 @@ $(function () {
             'russian',
             'wasabi'
         ];
-        for(var o in fonts){
+        for (var o in fonts) {
             var image = path + '/' + fontType + '/' + fonts[o] + '.png';
             var woff = path + '/' + fontType + '/' + fonts[o] + '.woff';
-            if(o==0){
+            if (o == 0) {
                 $('img', '.design-dropdown-fontfamily').attr('src', image);
             }
-            var item = $('<a class="font-families-item">').css('backgroundImage', 'url('+image+')').attr('data-image', image).attr('data-woff', woff);
+            var item = $('<a class="font-families-item">').css('backgroundImage', 'url(' + image + ')').attr('data-image', image).attr('data-woff', woff);
             $('.font-families').append(item);
         }
-        $('.font-families-item').click(function(){
+        $('.font-families-item').click(function () {
             var image = $(this).attr('data-image');
             $('img', '.design-dropdown-fontfamily').attr('src', image);
             $(this).parents('.design-dropdown').find('.design-dropdown-menu').hide();
         });
     }
 
-    function initColorPicker(){
+    function initColorPicker() {
         var colors = [
             '黑色,#000000',
             '白色,#FFFFFF',
@@ -140,20 +140,61 @@ $(function () {
             '紫红色,#672E45',
             '深黄色,#F6D400'
         ];
-        for(var o in colors){
+        for (var o in colors) {
             var name = colors[o].split(',')[0];
             var value = colors[o].split(',')[1];
             var item = $('<a class="color-picket-item" title="' + name + '"><span style="background: ' + value + ';"></span></a>');
             $('.color-picket-list').append(item);
         }
-        $('.color-picket-item').click(function(){
+        $('.color-picket-item').click(function () {
             var bgColor = $('span', this).css('backgroundColor');
             $(this).parents('.design-dropdown').find('.design-dropdown-color>span').css('backgroundColor', bgColor);
             $(this).parents('.design-dropdown').find('.design-dropdown-menu').hide();
         });
     }
 
+    function initProductCategories() {
+        var categories = [
+            {
+                id: 1,
+                name: '款式类型1'
+            },
+            {
+                id: 2,
+                name: '款式类型2'
+            },
+            {
+                id: 3,
+                name: '款式类型3'
+            }
+        ];
+        for (var o in categories) {
+            var c = categories[o];
+
+        }
+        initProducts();
+    }
+
+    function initProducts(products) {
+
+    }
+
+    $('.more-color', '.product-color-picket').hover(function () {
+        $(this).parents('.product-color-picket').find('.color-column').not('.quick-colors').show();
+        var len = $(this).parents('.product-color-picket').find('.color-column').length;
+        $(this).parents('.product-color-picket').width(len * 28);
+    });
+    $('.product-color-picket').mouseleave(function () {
+        $(this).find('.color-column').not('.quick-colors').hide();
+        $(this).width('auto');
+    });
+
+    //初始化设计工具数据
     initFontFamilies();
     initColorPicker();
+
+    //初始化产品类型
+    initProductCategories();
+    //初始化设计页高度
     setDsHeight();
 });
