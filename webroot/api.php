eb26a9c0ff1aa80468e9bef049ee758e0036963c
ihonestlife <haolun0228@qq.com>
2015-09-29 01:10:08 +0800

    静态

--- webroot/api.php
+++ webroot/api.php
@@ -1,12 +1,1 @@
-"
-<div class="product-color-picket"><span class="product-color-menu-arrow"></span>
-    <ul class="color-column quick-colors">
-        <li class="color-item" title="浅粉色"><span style="background-color: #DEB7CA;"></span></li>
-        <li class="color-item" title="栗色"><span style="background-color: #582D40;"></span></li>
-        <li class="color-item" title="红色"><span style="background-color: #B7312C;"></span></li>
-        <li class="color-item" title="橙色"><span style="background-color: #DD4814;"></span></li>
-        <li class="color-item" title="雏菊色"><span style="background-color: #FCD450;"></span></li>
-        <li class="color-item" title="爱尔兰绿"><span style="background-color: #00985F;"></span></li>
-        <li class="color-item" title="森林绿"><span style="background-color: #203731;"></span></li>
-    </ul>
-    "
+<?php
