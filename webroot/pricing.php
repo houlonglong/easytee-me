03c46c34552fa6a1eca53686d57e69dd9a737c10
ihonestlife <haolun0228@qq.com>
2015-10-10 16:56:28 +0800

    静态～

--- webroot/pricing.php
+++ webroot/pricing.php
@@ -5,30 +5,9 @@
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="css/common/style.css">
     <link rel="stylesheet" type="text/css" href="css/design/style.css"/>
-    <link rel="stylesheet" type="text/css" href="css/design/design.css"/>
+    <link rel="stylesheet" type="text/css" href="css/design/pricing.css"/>
     <script src="js/app/design/vendor/jquery-1.11.2.js"></script>
-    <script src="js/app/design/main.js"></script>
 </head>
-<header>
-    <div class="inHead">
-        <nav class="banxin clearfix nav">
-            <h1 class="logo">
-                <a href="index.html">易衫网</a>
-            </h1>
-            <span class="how">如何开始</span>
-
-            <div class="login">
-                <a href="#" class="begin">发起活动</a>
-
-                <div>
-                    <a href="#">消息</a>
-                    <a href="#">登录</a>
-                    <a href="#">注册</a>
-                </div>
-            </div>
-        </nav>
-    </div>
-</header>
 <div class="design-main">
     <div class="design-top">
         <div class="design-top-leaf"></div>
@@ -44,46 +23,193 @@
         </div>
     </div>
     <div class="design-center">
-
-    </div>
-</div>
-<div class="foot">
-    <div class="infooter clearfix">
-        <h2>易衫网</h2>
-        <dl>
-            <dt>导航</dt>
-            <dd>
-                <a href="#">首页</a>
-            </dd>
-            <dd>
-                <a href="#">开始设计</a>
-            </dd>
-            <dd>
-                <a href="#">关于我们</a>
-            </dd>
-            <dd>
-                <a href="#">帮助中心</a>
-            </dd>
-        </dl>
-        <dl>
-            <dt>服务</dt>
-            <dd>客服QQ：12345678</dd>
-        </dl>
-        <dl>
-            <dt>联系我们</dt>
-            <dd>工作日：上午9点 - 下午6点</dd>
-            <dd>休息日：上午9点 - 下午5点</dd>
-            <dd>客服热线 ： 400-92020-85</dd>
-        </dl>
-        <dl>
-            <dt>官方</dt>
-            <dd class="weibo">
-                <a href="#">微博</a>
-            </dd>
-            <dd class="weixin">微信</dd>
-        </dl>
-        <span class="ewm"></span>
-        <p>Copyright © 2014-2015 易衫网 沪公网备310107100040719</p>
+        <div class="ds-pricing-left">
+            <div class="ds-pricing-row">
+                <div class="ds-pricing-row-label">设定目标</div>
+                <div id="saleScroll"></div>
+            </div>
+            <div class="ds-pricing-row">
+                <div class="ds-pricing-row-left-panel">
+                    <input class="sale-goal" type="text"/>
+                    <span class="sale-goal-unit">件</span>
+                </div>
+                <div class="ds-pricing-row-right-panel">
+                    <p>
+                        目标是你希望能达到的销售目标，超过目标可以继续销售，不过你的成本将按照你设定的销售目标计算。如果没有达到目标，只要达到10件，且你设定的价格高于实际成本，我们仍将为你生产。
+                    </p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="ds-pricing-row dotted-top-line">
+                <div class="ds-pricing-row-left-panel">
+                    <div class="ds-pricing-row-left-panel-label">预计利润</div>
+                </div>
+                <div class="ds-pricing-row-right-panel">
+                    <div class="total-profit">
+                        <span class="money-unit-cn">¥</span>
+                        <span class="money-num">2,555.73+</span>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="ds-pricing-row dotted-top-line">
+                <div class="ds-pricing-row-label">
+                    设定售价
+                </div>
+                <div class="product-list">
+                    <div class="product-item product-item-default">
+                        <div class="product-item-icon"></div>
+                        <div class="product-item-left">
+                            <img class="product-image" src=""/>
+                        </div>
+                        <div class="product-item-right">
+                            <div class="product-item-pricing-info">
+                                <div class="product-item-right-column">
+                                    <div class="product-item-right-column-label">
+                                        成本：
+                                    </div>
+                                    <div class="product-item-right-column-info">
+                                        ¥
+                                        <span class="product-item-right-column-info-num">35.79</span>
+                                    </div>
+                                </div>
+                                <div class="product-item-right-column product-item-right-column-sign">
+                                    <span>+</span>
+                                </div>
+                                <div class="product-item-right-column">
+                                    <div class="product-item-right-column-label">
+                                        利润：
+                                    </div>
+                                    <div class="product-item-right-column-info">
+                                        ¥
+                                        <span class="product-item-right-column-info-num">35.79</span>
+                                    </div>
+                                </div>
+                                <div class="product-item-right-column product-item-right-column-sign">
+                                    <span>=</span>
+                                </div>
+                                <div class="product-item-right-column">
+                                    <div class="product-item-right-column-label" style="text-align: center;">
+                                        售价：
+                                    </div>
+                                    <div class="product-item-right-column-info">
+                                        <span class="product-item-right-column-money-unit">¥</span>
+                                        <input type="text" class="product-item-right-column-input"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product-item-color-info">
+                                <span class="product-item-color-info-label">已选颜色</span>
+                                <div class="product-item-selected">
+                                    <div class="product-item-color product-item-color-default">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                    <div class="product-item-color">
+                                        <span class="product-item-color-inner"></span>
+                                        <span class="product-item-color-delete"></span>
+                                    </div>
+                                </div>
+                                <div class="product-item-color-dropdown">
+                                    <div class="product-item-color-btn"></div>
+                                    <div class="product-item-color-menu">
+                                        <div class="product-item-color-menu-arrow"></div>
+                                        <div class="product-item-color-menu-color-list">
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                            <a class="product-item-color-menu-color-item" title="黑色" data-color="#000000"><span style="background: #000000;"></span></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <a class="product-item-delete" href="#"></a>
+                    </div>
+                </div>
+                <div class="product-add">
+                    <div class="product-add-icon"></div>
+                    <div class="product-add-total">还可以添加 <span>9</span> 种商品</div>
+                    <div class="product-add-tool">
+                        <div class="product-add-control">
+                            <div class="product-add-label">款式选择</div>
+                            <div class="product-add-select"></div>
+                        </div>
+                        <div class="product-add-control">
+                            <div class="product-add-label">产品选择</div>
+                            <div class="product-add-select"></div>
+                        </div>
+                        <div class="product-add-control">
+                            <div class="product-add-btn">添加</div>
+                        </div>
+                    </div>
+                    <div class="product-add-desc">
+                        添加更多的品类，让你的产品选择更加丰富
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ds-pricing-right">
+            <div class="ds-pricing-product-view">
+                <div id="ds_preview"></div>
+            </div>
+            <div class="ds-pricing-product-sides">
+                <a class="ds-pricing-products-side" href="#">正面</a>
+                <a class="ds-pricing-products-side" href="#">反面</a>
+                <a class="ds-pricing-products-side" href="#">左袖</a>
+                <a class="ds-pricing-products-side" href="#">右袖</a>
+            </div>
+        </div>
     </div>
 </div>
 </body>
