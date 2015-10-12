b291ac2fb0f9e1a4c4f609a9eca5c6d68188f53b
wujinrong@easytee.me <wujinrong@easytee.me>
2015-10-12 19:49:52 +0800

    支付页面样式

--- webroot/block/header_header.php
+++ webroot/block/header_header.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" type="text/css" href="css/common/style.css">
-<link rel="stylesheet" type="text/css" href="css/common/popup.css">
-<script type="text/javascript" src="js/app/common/jquery-1.11.2.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/common/style.css">
+<link rel="stylesheet" type="text/css" href="/css/common/popup.css">
+<script type="text/javascript" src="/js/app/common/jquery-1.11.2.min.js"></script>
 <script type="text/javascript" src="/js/libs/crypt/sha1.js"></script>
-<script type="text/javascript" src="js/app/common/popup.js"></script>
-<script type="text/javascript" src="js/app/common/index.js"></script>
+<script type="text/javascript" src="/js/app/common/popup.js"></script>
+<script type="text/javascript" src="/js/app/common/index.js"></script>
