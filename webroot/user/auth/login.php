90c6580395bf6b84e65ce314fdfad4ea79f5ab6e
wujinrong@easytee.me <wujinrong@easytee.me>
2015-10-13 11:45:56 +0800

    尾部链接

--- webroot/user/auth/login.php
+++ webroot/user/auth/login.php
@@ -43,46 +43,7 @@
 <!-- <input type="password" id="tel">   //判断手机号的正则
 <button id="btnJ">提交</button> -->
 
+<?php include(block("block/footer")) ?>
 
-<div class="foot">
-    <div class="infooter clearfix">
-        <h2>易衫网</h2>
-        <dl>
-            <dt>导航</dt>
-            <dd>
-                <a href="#">首页</a>
-            </dd>
-            <dd>
-                <a href="#">开始设计</a>
-            </dd>
-            <dd>
-                <a href="#">关于我们</a>
-            </dd>
-            <dd>
-                <a href="#">帮助中心</a>
-            </dd>
-        </dl>
-        <dl>
-            <dt>服务</dt>
-            <dd>客服QQ：12345678</dd>
-        </dl>
-        <dl>
-            <dt>联系我们</dt>
-            <dd>工作日：上午9点 - 下午6点</dd>
-            <dd>休息日：上午9点 - 下午5点</dd>
-            <dd>客服热线 ： 400-92020-85</dd>
-        </dl>
-        <dl>
-            <dt>官方</dt>
-            <dd class="weibo">
-                <a href="#">微博</a>
-            </dd>
-            <dd class="weixin">微信</dd>
-        </dl>
-        <span class="ewm"></span>
-
-        <p>Copyright © 2014-2015 易衫网 沪公网备310107100040719</p>
-    </div>
-</div>
 </body>
 </html>
